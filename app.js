06b769fc63b4c849b85b0be43a139434e93c6849
Derek Alia <derekalia@me.com>
2017-05-27 14:16:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to console
+app.use(logger('dev'));
+
+// Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome msg
+// in json format
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of something',
+}));
+
+module.exports = app;
+
